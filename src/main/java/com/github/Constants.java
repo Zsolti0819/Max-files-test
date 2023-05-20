aadeec72523ad0bff640fe8cd11659a93dc44e3d
Zsolti0819 <kiss.zsolt0819@gmail.com>
2023-05-21 00:07:46 +0200

    Refactoring

--- src/main/java/com/github/Constants.java
+++ src/main/java/com/github/Constants.java
@@ -1,17 +1,15 @@
 package com.github;
 
-import lombok.experimental.UtilityClass;
-
-import java.io.File;
 import java.nio.file.Path;
+import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Constants {
 
-    private static final String BASE_PATH = System.getProperty("user.dir");
-    public static final Path RESOURCES_PATH = Path.of(BASE_PATH + File.separator + "src" + File.separator + "main" + File.separator + "resources");
-    public static final Path TXT_OUTPUT_FOLDER = Path.of(RESOURCES_PATH + File.separator + "txt");
-    public static final Path LOREM_FILE = Path.of(RESOURCES_PATH + File.separator + "lorem.txt");
+    private static final String BASE_PATH = String.valueOf(Path.of("").toAbsolutePath());
+    public static final Path RESOURCES_PATH = Path.of(BASE_PATH, "src", "main", "resources");
+    public static final Path TXT_OUTPUT_FOLDER = RESOURCES_PATH.resolve("txt");
+    public static final Path LOREM_FILE = RESOURCES_PATH.resolve("lorem.txt");
     public static final int NUMBER_OF_FILES = 1000000;
 
 }
--- src/main/java/com/github/Main.java
+++ src/main/java/com/github/Main.java
@@ -8,31 +8,31 @@ import java.nio.file.Files;
 public class Main {
 
     private static File[] createFiles() throws IOException {
-        File[] files = new File[Constants.NUMBER_OF_FILES];
+        final File[] files = new File[Constants.NUMBER_OF_FILES];
         Files.createDirectories(Constants.TXT_OUTPUT_FOLDER);
         for (int i = 0; i < Constants.NUMBER_OF_FILES; i++) {
-            File file = new File(Constants.TXT_OUTPUT_FOLDER + File.separator + "file_" + i + ".txt");
+            final File file = new File(Constants.TXT_OUTPUT_FOLDER + File.separator + "file_" + i + ".txt");
             if (file.createNewFile()) {
                 files[i] = file;
-            }
-            else {
+            } else {
                 System.out.println("The file already exists.");
             }
         }
         return files;
     }
-    public static void main(String[] args) throws IOException {
 
-        String longLoremIpsum = String.valueOf(Files.readString(Constants.LOREM_FILE));
-        File[] files = createFiles();
-        PrintStream[] fileWriters = new PrintStream[Constants.NUMBER_OF_FILES];
+    public static void main(final String[] args) throws IOException {
+
+        final String longLoremIpsum = String.valueOf(Files.readString(Constants.LOREM_FILE));
+        final File[] files = createFiles();
+        final PrintStream[] fileWriters = new PrintStream[Constants.NUMBER_OF_FILES];
 
         for (int i = 0; i < Constants.NUMBER_OF_FILES; i++) {
             // Not using try-with-resources on purpose
             try {
                 fileWriters[i] = new PrintStream(Constants.TXT_OUTPUT_FOLDER + File.separator + files[i].getName());
                 fileWriters[i].print(longLoremIpsum);
-            } catch (IOException e) {
+            } catch (final IOException e) {
                 e.printStackTrace();
             }
         }
